6a1187ef0c334c705bd33b14110c733554230e54
Omar <nyirurugoomar@gmail.com>
2024-08-22 15:34:37 +0200

    Adding changes in middleware

--- controllers/authMiddleware.ts
+++ controllers/authMiddleware.ts
@@ -6,14 +6,14 @@ export async function authMiddleware(req: NextRequest) {
     const tokenCookie = req.cookies.get('token');
 
     if (!tokenCookie) {
-        return NextResponse.json({ message: 'Please Login Before You Post Job' }, { status: 401 });
+        return NextResponse.json({ message: 'Please Login/SignUp Before You Post Job' }, { status: 401 });
     }
 
     const token = tokenCookie.value; // Extract the token value
 
     try {
         const decoded = jwt.verify(token, process.env.TOKEN_SECRET!);
-        (req as any).user = decoded; // Add user info to the request object
+        (req as any).user = decoded; 
     } catch (error) {
         return NextResponse.json({ message: 'Unauthorized: Invalid token' }, { status: 401 });
     }
